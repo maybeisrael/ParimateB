45bbfcd7c018a3e8008c48604dd30a2ea5b39ff1
NFORSHIFU234Dev || Shifu-Nfor Nyuiring-yoh Rhagninyui <nforshifu234.dev@gmail.com>
2024-05-23 16:58:42 +0100

    You need to write this your code well. Because it was hard to navigate trough. Also the design you wanted was annoying so i added mine

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -3,108 +3,110 @@ import Image from 'next/image';
 
 export default function Content() {
   return (
-    <div className="relative min-h-screen backgroundd">
-      <div className="absolute left-0 bottom-0 w-full sm:w-3/4 md:w-1/2 lg:w-1/3">
-        <Image
-          src="/images/hero.png"
-          alt="hero image"
-          layout="responsive"
-          width={699}
-          height={695}
-        />
-      </div>
+    <div className="relative min-h-scree backgroundd mb-32">
 
-      <div className="flex flex-col items-center justify-center py-7 text-center min-h-screen">
-        <div className="box w-full sm:w-5/6 md:w-4/5 lg:w-3/4 xl:w-2/3 p-4 rounded-md">
-          <div className="text-xl md:text-2xl lg:text-5xl">
+
+      <div className=" py-7 text-center relative flex flex-col items-center">
+
+        <div className="box w-100 p-1 rounded-md w-[80%] ">
+
+          <div className="text-xl font-bold">
             <span className="text-white">High winning Odds-</span>
             <span className="text-blue-500">Hedge </span>
             <span className="text-white">your bet</span>
           </div>
+
         </div>
 
-        <div className="flex-content mt-5">
+        <div className="flex-content mt-5 w-[40%] text-white text-xs">
           Hedge your bets by joining multiple stakes. Explore alternative outcomes and increase your chances of winning!
         </div>
 
-        <div className="mt-5 w-full flex justify-end">
-          <ul className="flex flex-col items-end">
+        <div className="w-100 flex-col lg:flex-row flex justify-center mt-4 items-center border2 border-red-500 bg-[#2A2D37E5] lg:h-screen relative  w-[80%] mx-0 top-0  rounded-lg shadow-lg drop-shadow shadow-white">
+
+          <div className="relative lg:absolute left-0 bottom-0 w-full sm:w-3/4 md:w-1/2 lg:w-">
+            <Image
+              src="/images/hero.png"
+              alt="hero image"
+              className='-mb-16'
+              width={1000}
+              height={695}
+              
+            />
+          </div>
+
+          <ul className="flex flex-col  text-white text-left py-14">
+
             <li
-              className="flex items-center py-5"
-              style={{
-                width: '827px',
-                height: '102px',
-                gap: '0px',
-                opacity: '0',
-              }}
+              className="flex items-center  relative lg:absolute lg:left-[20%] top-[10%]"
             >
-              <div className="badge flex items-center mr-2">
+              <div className="badge">
                 <Image
                   src="/images/Group.png"
                   alt="badge"
-                  width={80}
-                  height={70}
+                  width={100}
+                  height={0}
                 />
               </div>
-              <div className="flex flex-col">
-                <div>Download Parimate</div>
-                <div>With PariMate, it&apos;s your game, your way. Connect with fellow betting enthusiasts and build your network.</div>
+              <div className="flex flex-col pt-6 -ml-9">
+                <div className='font-bold text-lg'>Download Parimate</div>
+                <div className='text-[12px] text-gray-400'>With PariMate, it&apos;s your game, your way. <br /> Connect with fellow betting enthusiasts and build your network.</div>
               </div>
             </li>
+
             <li
-              className="flex items-center py-5"
-              style={{
-                width: '537px',
-                height: '102px',
-                padding: '0px 6px 0px 0px',
-                gap: '0px',
-                opacity: '0',
-              }}
+              className="flex items-center relative lg:absolute lg:left-[45%] top-[27%]"
             >
-              <div className="badge flex items-center mr-2">
+              <div className="badge">
                 <Image
                   src="/images/Group.png"
                   alt="badge"
-                  width={80}
-                  height={70}
+                  width={100}
+                  height={0}
                 />
               </div>
-              <div className="flex flex-col">
-                <div>Fund your wallet</div>
-                <div>With PariMate, it&apos;s your game, your way. Connect with fellow betting enthusiasts and build your network.</div>
+              <div className="flex flex-col pt-6 -ml-9">
+                <div className='font-bold text-lg'>Fund your wallet</div>
+                <div className='text-[12px] text-gray-400'>With PariMate, it&apos;s your game, your way. <br /> Connect with fellow betting enthusiasts and build <br /> your network.</div>
               </div>
             </li>
-            <li className="flex items-center py-5">
-              <div className="badge flex items-center mr-2">
+
+            <li className="flex items-center relative lg:absolute lg:left-[52%] top-[50%]">
+          	   <div className="badge">
                 <Image
                   src="/images/Group.png"
                   alt="badge"
-                  width={80}
-                  height={70}
+                  width={100}
+                  height={0}
                 />
-              </div>
-              <div className="flex flex-col">
-                <div>Create or Join Game(s)</div>
-                <div>With PariMate, it&apos;s your game, your way. Connect with fellow betting enthusiasts and build your network.</div>
+               </div>
+              <div className="flex flex-col pt-6 -ml-9">
+                <div className='font-bold text-lg'>Create or Join Game(s)</div>
+                <div className='text-[12px] text-gray-400'>With PariMate, it&apos;s your game, your way. <br /> Connect with fellow betting enthusiasts and build <br /> your network.</div>
               </div>
             </li>
-            <li className="flex items-center py-5">
-              <div className="badge flex items-center mr-2">
+
+            <li className="flex items-center relative lg:absolute lg:left-[49%] top-[73%]">
+              <div className="badge">
                 <Image
                   src="/images/Group.png"
                   alt="badge"
-                  width={80}
-                  height={70}
+                  width={100}
+                  height={0}
                 />
               </div>
-              <div className="flex flex-col">
-                <div>Win Stake!</div>
-                <div>With PariMate, it&apos;s your game, your way. Connect with fellow betting enthusiasts and build your network.</div>
+              <div className="flex flex-col pt-6 -ml-9">
+                <div className='font-bold text-lg'>Download Parimate</div>
+                <div className='text-[12px] text-gray-400'>With PariMate, it&apos;s your game, your way. <br /> Connect with fellow betting enthusiasts and build your network.</div>
               </div>
             </li>
+
           </ul>
+          
         </div>
+
       </div>
+
     </div>
   );
 }
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,20 +12,22 @@ const Footer = () => {
     <footer
       style={{
         background: '#2A2D37',
-        height: '322px', 
+        minHeight: '322px', 
       }}
-      className="footer flex p-8 text-white"
+      className="footer flex justify-between p-8 text-white lg:flex-row flex-col items-center w-100 m-0"
     >
       <div
+
         style={{
-          width: '70%', // Set the left side width to 70%
-          marginRight: '20px', // Add margin-right for space
-          top: '18px',
-          left: '88px',
+          // width: '70%', // Set the left side width to 70%
+          // marginRight: '20px', // Add margin-right for space
+          // top: '18px',
+          // left: '88px',
           position: 'relative',
-          marginLeft:'20px',
+          // marginLeft:'20px',
           
         }}
+        className='lg:w-[50%] mb-6'
       >
         <div className={`${inknut.className} text-lg`}>
           Peer<span className="text-blue-500">Bet</span>
@@ -37,13 +39,13 @@ const Footer = () => {
         </div>
       </div>
 
-      <div style={{ width: '25%' }}> 
-        <div className="flex justify-end">
-          <ul className="list-none">
+      <div > 
+        <div className="flex justify-en items-center">
+          <ul className="list-none flex items-center justify-center flex-wrap gap-x-3">
             <li>Pages</li>
-            <li className="mt-6">Game Rules</li>
-            <li className="mt-6">Terms & Conditions</li>
-            <li className="mt-6">Privacy Policy</li>
+            <li className="mt-">Game Rules</li>
+            <li className="mt-">Terms & Conditions</li>
+            <li className="mt-">Privacy Policy</li>
 
             <div className="flex items-center mt-6 space-x-2">
               <Image
